2b20502529c70e1347da71c42be1f9d6495ab145
moazyoure-wq <moazyoure@gmail.com>
2026-07-09 23:19:01 +0300

    Create page.jsx

---
+++ website/app/page.jsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h1>منصة الخوارزمي</h1>
+
+        <h2>مع المستر أمير طلعت</h2>
+
+        <p>
+          تعلم الرياضيات بطريقة سهلة ومبسطة
+        </p>
+
+        <button>
+          اشترك الآن
+        </button>
+
+        <button>
+          الكورسات
+        </button>
+      </section>
+    </main>
+  );
+}
